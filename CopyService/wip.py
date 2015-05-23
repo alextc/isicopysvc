1a9aa9eb212a0d0a51c8e03d520fc739d4bef896
JeromySt <thelotrs@hotmail.com>
2015-05-22 18:41:40 -0700

    never recursive spawn for now, let Cron kick off more if needed

--- CopyService/wip.py
+++ CopyService/wip.py
@@ -589,7 +589,6 @@ try:
                 perform_heartbeat(my_work)
                 spawn_new_worker(False)
                 process_work(my_work)
-    spawn_new_worker(False)
 except KeyboardInterrupt:
     Logger.log_message("ctrl-c detected")
 except Exception as e:
