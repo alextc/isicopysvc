f51d6125abcd41995766b4a9f80a63eaacef8d6b
alextc <alextc@microsoft.com>
2015-06-12 10:23:10 -0700

    Refactored Phase2Calc to use path.join instead of string concat.

--- CopyService/model/phase2pathcalculator.py
+++ CopyService/model/phase2pathcalculator.py
@@ -8,9 +8,9 @@ import os
 # https://docs.python.org/2/library/string.html
 
 class Phase2PathCalculator(object):
-    _copy_service_root = "/ifs/zones/"
-    _copy_service_from_sub_path = "/copy_svc/from/"
-    _copy_service_to_sub_path = "/copy_svc/to/"
+    _copy_service_root = "/ifs/zones"
+    _copy_service_from_sub_path = "copy_svc/from"
+    _copy_service_to_sub_path = "copy_svc/to"
 
     def __init__(self, phase2_source_dir):
         assert os.path.exists(Phase2PathCalculator._copy_service_root), \
@@ -18,17 +18,24 @@ class Phase2PathCalculator(object):
 
         self._from_zone = phase2_source_dir.split('/')[3]
 
-        # TODO: "/" does not need to be there - how did it work so far?
-        assert os.path.exists(Phase2PathCalculator._copy_service_root + "/" + self._from_zone), \
-            "Directory for from_zone {0} not found}".format(self._from_zone)
-        assert os.path.exists(Phase2PathCalculator._copy_service_root + "/" + self._from_zone + Phase2PathCalculator._copy_service_to_sub_path), \
-            "Directory for from_zone {0} to_path not found}".format(self._from_zone)
+        from_zone_path = os.path.join(Phase2PathCalculator._copy_service_root, self._from_zone)
+        assert os.path.exists(from_zone_path), "Directory for from_zone {0} not found}".format(from_zone_path)
+
+        from_zone_copy_svc_to_path = os.path.join(from_zone_path, Phase2PathCalculator._copy_service_to_sub_path)
+
+        assert os.path.exists(from_zone_copy_svc_to_path), \
+                "Directory for from_zone_copy_svc_to_path: {0} not found".format(from_zone_copy_svc_to_path)
 
         self._to_zone = phase2_source_dir.split('/')[6]
-        assert os.path.exists(Phase2PathCalculator._copy_service_root + "/" + self._to_zone), \
-            "Directory for to_zone {0} not found}".format(self._to_zone)
-        assert os.path.exists(Phase2PathCalculator._copy_service_root + "/" + self._to_zone + Phase2PathCalculator._copy_service_from_sub_path), \
-            "Directory for to_zone {0} from_path not found}".format(self._from_zone)
+        assert os.path.exists(os.path.join(
+            Phase2PathCalculator._copy_service_root,
+            self._to_zone)), \
+                "Directory for to_zone {0} not found}".format(self._to_zone)
+        assert os.path.exists(os.path.join(
+            Phase2PathCalculator._copy_service_root,
+            self._to_zone,
+            Phase2PathCalculator._copy_service_from_sub_path)), \
+                "Directory for to_zone {0} from_path not found}".format(self._from_zone)
 
         self._phase2_source_dir = phase2_source_dir
         # Removing this Assert - in a multi threaded scenario it is quite possible to this directory not to exist
@@ -40,21 +47,20 @@ class Phase2PathCalculator(object):
         self._dir_name = os.path.split(self._phase2_source_dir)[1]
 
     def get_phase2_target_dir(self):
-        return Phase2PathCalculator._copy_service_root + \
-               self._to_zone + \
-               Phase2PathCalculator._copy_service_from_sub_path + \
-               self._from_zone + "/" + \
-               self._dir_name
+        return os.path.join(Phase2PathCalculator._copy_service_root,
+                            self._to_zone,
+                            Phase2PathCalculator._copy_service_from_sub_path,
+                            self._from_zone,
+                            self._dir_name)
 
     def get_phase1_source_dir(self):
-        phase1_source_dir = \
-            Phase2PathCalculator._copy_service_root + \
-            self._from_zone + \
-            Phase2PathCalculator._copy_service_to_sub_path + \
-            self._to_zone + "/" + \
-            self._dir_name
+        phase1_source_dir = os.path.join(
+            Phase2PathCalculator._copy_service_root,
+            self._from_zone,
+            Phase2PathCalculator._copy_service_to_sub_path,
+            self._to_zone,
+            self._dir_name)
 
-        # Uncomment this once testing on phase2 is complete
         assert not os.path.exists(phase1_source_dir), \
             "We are in phase2 but phase1_source_dir {0} still exist".format(phase1_source_dir)
 
