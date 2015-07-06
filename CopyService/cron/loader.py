7f008307220ddfb3319c7587e7ca2cde13e3ff8e
alextc <alextc@microsoft.com>
2015-07-06 10:24:46 -0700

    BUG Fix: SetAclCommand was clearing the log on every instantiation of the object. Changed logger variable to Class Static

---
+++ CopyService/cron/loader.py
@@ -0,0 +1,174 @@
+__author__ = 'Kailash Joshi'
+import os
+import ConfigParser
+import datetime
+import logging
+from subprocess import Popen, PIPE
+import sys
+import re
+
+CONFIG_FILE = "/ifs/copy_svc/config"
+LOOK_FOR = None
+PARENT_LOG_PATH = None
+LOG_FILENAME = None
+PATH_TO_LOG = None
+STALE_HEARTBEAT = None
+ROTATE_FREQUENCY = None
+HEARTBEAT_LOG = '/ifs/copy_svc/find_dirs_hb.dat'
+
+
+def set_config():
+    """
+    Read configuration file
+    :return: None
+    """
+    conf = ConfigParser.ConfigParser()
+    try:
+        with open(CONFIG_FILE) as f:
+            conf.readfp(f)
+    except IOError, i:
+        raise i
+    global LOOK_FOR, PARENT_LOG_PATH, LOG_FILENAME, PATH_TO_LOG, STALE_HEARTBEAT, ROTATE_FREQUENCY
+    LOOK_FOR = conf.get('Check_running_one_node', 'Process')
+    PARENT_LOG_PATH = conf.get('Check_running_one_node', 'LogPath')
+    LOG_FILENAME = conf.get('Check_running_one_node', 'LogFile')
+    PATH_TO_LOG = os.path.join(PARENT_LOG_PATH, LOG_FILENAME)
+    logging.basicConfig(filename=PATH_TO_LOG, level=logging.DEBUG,
+                        format='%(asctime)s.%(msecs)d %(message)s',
+                        datefmt="%Y,%m,%d,%H,%M,%S")
+    STALE_HEARTBEAT = conf.get('Check_running_one_node', 'Stale')
+    ROTATE_FREQUENCY = conf.get('Check_running_one_node', 'Rotate')
+
+
+def get_date():
+    """
+    Get UTC date and time in a specific format
+    :return: Comma separated date and time
+    """
+    return datetime.datetime.utcnow().strftime("%Y, %m, %d, %H, %M, %S")
+
+
+def get_last_heartbeat():
+    """
+    Read last line of the log file
+    :return: timestamp (2015, 06, 23, 06, 29, 43)
+    """
+    with open(HEARTBEAT_LOG, 'r') as f:
+        return f.readlines()[-1].rstrip()
+
+
+def get_heartbeat_diff():
+    """
+    Difference in seconds
+    :return: integer (seconds)
+    """
+    current = datetime.datetime.strptime(get_date(), '%Y, %m, %d, %H, %M, %S')
+    last_record = datetime.datetime.strptime(get_last_heartbeat(), '%Y, %m, %d, %H, %M, %S')
+    return (current - last_record).seconds
+
+
+def get_running():
+    """
+    List all the  processes running.
+    :return: list of processes
+    """
+    process = Popen(
+        ['isi_for_array', '-X', 'ps', 'auxwww', '|grep', LOOK_FOR, '|grep', '-v', 'grep|grep', '-v',
+         '"sh -c /usr/bin/python {0}"'.format(LOOK_FOR), '|grep', '-v', 'isi_rdo|grep', '-v', 'isi_for_array|grep',
+         '-v',
+         'ropc'], stdout=PIPE, stderr=PIPE)
+    communicate = process.communicate()
+    return filter(None, communicate[0].split('\n'))
+
+
+def kill_all():
+    """
+    Kill all the running processes
+    :return: None
+    """
+    cmd = "isi_for_array ps auxwww|grep {0} |grep -v grep|".format(LOOK_FOR) + r"awk '{print$3}'"
+    process = Popen(['/bin/bash', '-c', cmd], stdout=PIPE, stderr=PIPE)
+    communicate = filter(None, process.communicate()[0].split('\n'))
+    null = open(os.devnull, 'w')
+
+    for i in communicate:
+        Popen(['/bin/bash', '-c', '/usr/bin/isi_for_array /bin/kill {0} '.format(i)], stderr=null,
+              stdout=null).communicate()
+
+
+def start_process():
+    """
+    Start/Restart new process
+    :return: None
+    """
+    logging.info("Starting {0} now".format(LOOK_FOR))
+    Popen(['/bin/bash', '-c', '/usr/bin/isi_ropc /usr/bin/python {0} & disown'.format(LOOK_FOR)])
+    return
+
+
+def get_frequency(process_list):
+    """
+    Get number of processes running
+    :param process_list: list of processes
+    :return: Number of processes
+    """
+    first_item = process_list[0]
+    if 'exited with status 1' in first_item:
+        return 0
+    else:
+        return len(process_list)
+
+
+def test_ok(running, frequency, last_heartbeat, last_heartbeat_diff):
+    """
+    Check if the processing is running properly
+    :param running:
+    :param frequency:
+    :param last_heartbeat:
+    :param last_heartbeat_diff:
+    :return: None
+    """
+    if frequency == 1 and last_heartbeat_diff < int(STALE_HEARTBEAT):
+        logging.info('Found {0} with current heartbeat {1}'.format(LOOK_FOR, last_heartbeat))
+        logging.info(running[0])
+        print 'Test Passed'
+        sys.exit(0)
+
+
+def main():
+    set_config()
+    running = get_running()
+    frequency = get_frequency(running)
+    last_heartbeat = get_last_heartbeat()
+    last_heartbeat_diff = get_heartbeat_diff()
+    test_ok(running, frequency, last_heartbeat, last_heartbeat_diff)
+    if frequency == 0:
+        logging.info('{0} not found anywhere.  Starting now...'.format(LOOK_FOR))
+    elif frequency > 1:
+        logging.info("Too many {0} running.".format(LOOK_FOR))
+        logging.info("PS output ---> {0} < ---".format(running))
+        logging.info("Killing all processes across the cluster.")
+        kill_all()
+    elif last_heartbeat_diff > int(STALE_HEARTBEAT):
+        logging.info("{0} running with stale heartbeat {1}".format(LOOK_FOR, last_heartbeat))
+        logging.info("PS output ---> {0} < ---".format(running))
+        logging.info("Killing {0} on {1}".format(LOOK_FOR, running))
+        null = open(os.devnull, 'w')
+        for i in running:
+            _all_data = i.split()
+            _node_id = re.findall('\d+', _all_data[0])[0]
+            _pid = _all_data[2]
+            Popen(['/bin/bash', '-c', '/usr/bin/isi_for_array -n {0} /bin/kill {1} '.format(_node_id, _pid)],
+                  stderr=null, stdout=null).communicate()
+
+    else:
+        logging.info("Something is unexpected.")
+        logging.info("PS output ---> {0} < ---".format(running))
+        logging.info("Last heartbeat:  {0}".format(last_heartbeat))
+    start_process()
+    restart = get_running()
+    logging.info('PS output ---> {0} <---'.format(restart[0]))
+
+
+if __name__ == '__main__':
+    main()
--- CopyService/isiapi/setaclcommand.py
+++ CopyService/isiapi/setaclcommand.py
@@ -8,11 +8,12 @@ class SetAclCommand(NamespaceCommand):
     _function_call_count = 0
     _retry_max_count = 10
     _retry_back_off_in_sec = 1
+    _logger = LoggerFactory.create('SetAclCommand')
 
     def __init__(self, path, acl):
-        self._logger = LoggerFactory.create(SetAclCommand.__name__)
-        self._logger.debug("Path to ACL:{0}".format(path))
-        self._logger.debug("ACL to apply:\n{0}".format(acl))
+
+        SetAclCommand._logger.debug("Path to ACL:{0}".format(path))
+        SetAclCommand._logger.debug("ACL to apply:\n{0}".format(acl))
         NamespaceCommand.__init__(self, http_verb="PUT", directory=path, qry_dict={'acl': ''}, body_data=acl)
         self._path = path
 
@@ -21,15 +22,18 @@ class SetAclCommand(NamespaceCommand):
             SetAclCommand._function_call_count += 1
             try:
                 raw_response = super(SetAclCommand, self).execute()
-                self._logger.debug("Set ACL API for dir {0} completed with the status of {1} and message body of {2}".
+                SetAclCommand._logger.debug(
+                    "Set ACL API for dir {0} completed with the status of {1} and message body of {2}".
                     format(self._path, raw_response[0], raw_response[1]))
                 assert raw_response[0] == 200, "Set ACL API for dir {0} failed,response was {1}, message body was {2}".\
                     format(self._path, raw_response[0], raw_response[1])
                 return
             except RuntimeError as e:
-                self._logger.debug("{0}, while processing {1}".format(e, self._path))
-                self._logger.debug("The function was called {0} times".format(SetAclCommand._function_call_count))
+                SetAclCommand._logger.debug(
+                    "{0}, while processing {1}".format(e, self._path))
+                SetAclCommand._logger.debug(
+                    "The function was called {0} times".format(SetAclCommand._function_call_count))
                 time.sleep(SetAclCommand._retry_back_off_in_sec)
 
-        self._logger.debug("Maximum retry count reached; failing")
+        SetAclCommand._logger.debug("Maximum retry count reached; failing")
         raise RuntimeError("Unable to complete SetACLCommand")
