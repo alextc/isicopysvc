34ff5ca3bc6a06efd71d974aa2871b79e71ef617
alextc <alextc@microsoft.com>
2015-07-06 13:22:02 -0700

    Added SqliteFacade Class. Refactored Phase2Db to delegate Sqlite calls to SqliteFacade.

--- CopyService/sql/phase2db.py
+++ CopyService/sql/phase2db.py
@@ -2,27 +2,24 @@ __author__ = 'alextc'
 import sqlite3
 import os
 from model.phase2workitem import Phase2WorkItem
+from sqlitefacade import SqliteFacade
 import logging
 
 
 class Phase2Db:
     def __init__(self):
         self._data_file_path = "/ifs/copy_svc/phase2.db"
-        with sqlite3.connect(
-                self._data_file_path,
-                detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as connection:
-            cursor = connection.cursor()
-            cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='phase2_work_items'")
-            result = cursor.fetchone()
-            if not result:
-                cursor.execute("CREATE TABLE phase2_work_items "
-                               "(directory TEXT NOT NULL, "
-                               "directory_last_modified timestamp NOT NULL, "
-                               "host TEXT NOT NULL, "
-                               "pid TEXT NOT NULL, "
-                               "heartbeat timestamp NOT NULL, "
-                               "state TEXT NOT NULL, "
-                               "PRIMARY KEY (directory, directory_last_modified))")
+
+        command = "CREATE TABLE IF NOT EXISTS phase2_work_items " \
+                  "(directory TEXT NOT NULL, " \
+                  "directory_last_modified timestamp NOT NULL, " \
+                  "host TEXT NOT NULL, " \
+                  "pid TEXT NOT NULL, " \
+                  "heartbeat timestamp NOT NULL, " \
+                  "state TEXT NOT NULL, " \
+                  "PRIMARY KEY (directory, directory_last_modified))"
+
+        SqliteFacade.execute_command(self._data_file_path, command)
 
     def try_to_take_ownership(self, work_item):
         """
@@ -39,20 +36,18 @@ class Phase2Db:
             logging.debug("Returning False")
             return False
 
-        try:
-            with sqlite3.connect(
-                    self._data_file_path,
-                    detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as connection:
-                cursor = connection.cursor()
-                cursor.execute \
-                    ('INSERT INTO phase2_work_items (directory, directory_last_modified, host, pid, heartbeat, state) '
-                     'VALUES (?,?,?,?,?,?)',
-                     (work_item.phase2_source_dir,
+        command = 'INSERT INTO phase2_work_items (directory, directory_last_modified, host, pid, heartbeat, state) ' \
+                  'VALUES (?,?,?,?,?,?)'
+
+        parameters = (work_item.phase2_source_dir,
                       work_item.phase2_source_dir_last_modified,
                       work_item.host,
                       work_item.pid,
                       work_item.heartbeat,
-                      work_item.state))
+                      work_item.state)
+
+        try:
+            SqliteFacade.execute_parameterized_command(self._data_file_path, command, parameters)
         except sqlite3.IntegrityError as e:
             logging.debug(e)
             return False
@@ -66,20 +61,16 @@ class Phase2Db:
         """
         logging.debug("ENTERING remove_work_item")
         # logging.debug("Relieved work_item to remove:\n{0}".format(work_item))
-        try:
-            with sqlite3.connect(
-                    self._data_file_path,
-                    detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as connection:
-                cursor = connection.cursor()
-                cursor.execute('DELETE FROM phase2_work_items '
-                               'WHERE directory=(?) AND directory_last_modified=(?) AND host=(?) AND pid=(?)',
-                               (work_item.phase2_source_dir,
-                                work_item.phase2_source_dir_last_modified,
-                                work_item.host,
-                                work_item.pid))
-        except sqlite3.IntegrityError as e:
-            logging.debug(e)
-            raise
+
+        command = 'DELETE FROM phase2_work_items ' \
+                  'WHERE directory=(?) AND directory_last_modified=(?) AND host=(?) AND pid=(?)'
+
+        parameters = (work_item.phase2_source_dir,
+                      work_item.phase2_source_dir_last_modified,
+                      work_item.host,
+                      work_item.pid)
+
+        SqliteFacade.execute_parameterized_command(self._data_file_path, command, parameters)
 
     def write_heart_beat(self, work_item):
         """
@@ -88,24 +79,18 @@ class Phase2Db:
         """
         logging.debug("ENTERING")
         logging.debug("Received work_item to write:\n{0}".format(work_item))
-        try:
 
-            with sqlite3.connect(
-                    self._data_file_path,
-                    detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as connection:
-                cursor = connection.cursor()
-                cursor.execute ('INSERT OR REPLACE INTO phase2_work_items '
-                                '(directory, directory_last_modified, host, pid, heartbeat, state)'
-                                ' VALUES (?,?,?,?,?,?)',
-                                (work_item.phase2_source_dir,
-                                 work_item.phase2_source_dir_last_modified,
-                                 work_item.host,
-                                 work_item.pid,
-                                 work_item.heartbeat,
-                                 work_item.state))
-        except sqlite3.Error as e:
-            logging.debug(e.message)
-            raise
+        command = 'INSERT OR REPLACE INTO phase2_work_items ' \
+                  '(directory, directory_last_modified, host, pid, heartbeat, state) VALUES (?,?,?,?,?,?)'
+
+        parameters = (work_item.phase2_source_dir,
+                      work_item.phase2_source_dir_last_modified,
+                      work_item.host,
+                      work_item.pid,
+                      work_item.heartbeat,
+                      work_item.state)
+
+        SqliteFacade.execute_parameterized_command(self._data_file_path, command, parameters)
 
     # TODO: should not this be directory and the last_modified - otherwise this may return multiple items
     def get_heart_beat(self, directory):
@@ -113,13 +98,11 @@ class Phase2Db:
         :type directory: str
         :rtype: Phase2WorkItem
         """
-        with sqlite3.connect(
-                self._data_file_path,
-                detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as connection:
-            connection.row_factory = sqlite3.Row
-            cursor = connection.cursor()
-            cursor.execute('SELECT * FROM phase2_work_items WHERE directory = (?)', (directory,))
-            result = cursor.fetchone()
+
+        query = 'SELECT * FROM phase2_work_items WHERE directory = (?)'
+        parameters = (directory,)
+
+        result = SqliteFacade.execute_parameterized_select(self._data_file_path, query, parameters)
 
         # no matching record found
         if not result:
@@ -140,9 +123,5 @@ class Phase2Db:
         return heart_beat
 
     def clear_heart_beat_table(self):
-        with sqlite3.connect(
-                self._data_file_path,
-                detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as connection:
-            cursor = connection.cursor()
-            sql_delete_query = "DELETE from phase2_work_items"
-            cursor.execute(sql_delete_query)
+        command = "DELETE from phase2_work_items"
+        SqliteFacade.execute_command(self._data_file_path, command)
---
+++ CopyService/sql/sqlitefacade.py
@@ -0,0 +1,77 @@
+__author__ = 'alextc'
+import sqlite3
+from log.loggerfactory import LoggerFactory
+from datetime import datetime
+from common.datetimeutils import DateTimeUtils
+
+
+class SqliteFacade(object):
+
+    _logger = LoggerFactory.create('SqliteFacade')
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def execute_parameterized_select(db_path, select_statement, parameters):
+        SqliteFacade._log_sql_command(select_statement, parameters, db_path)
+
+        try:
+            with sqlite3.connect(db_path, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as connection:
+                connection.row_factory = sqlite3.Row
+                cursor = connection.cursor()
+                cursor.execute(select_statement, parameters)
+                return cursor.fetchall()
+        except sqlite3.Error as e:
+            SqliteFacade._logger.debug(e.message)
+            raise
+
+    @staticmethod
+    def execute_parameterized_command(db_path, command_statement, parameters):
+        SqliteFacade._log_sql_command(command_statement, parameters, db_path)
+
+        try:
+
+            with sqlite3.connect(db_path, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as connection:
+                connection.row_factory = sqlite3.Row
+                cursor = connection.cursor()
+                cursor.execute(command_statement, parameters)
+        except sqlite3.Error as e:
+            SqliteFacade._logger.debug(e.message)
+            raise
+
+    @staticmethod
+    def execute_command(db_path, command_statement):
+        SqliteFacade._log_sql_command(command_statement, "", db_path)
+
+        try:
+
+            with sqlite3.connect(db_path, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as connection:
+                connection.row_factory = sqlite3.Row
+                cursor = connection.cursor()
+                cursor.execute(command_statement)
+        except sqlite3.Error as e:
+            SqliteFacade._logger.debug(e.message)
+            raise
+
+    @staticmethod
+    def _log_sql_command(select_statement, parameters, db_path):
+        if parameters:
+            SqliteFacade._logger.debug(
+                "About to execute\n{0}\nwith parameters:{1}\nagainst:{2}".format(
+                    select_statement,
+                    SqliteFacade._parameters_tostring(parameters),
+                    db_path))
+        else:
+            SqliteFacade._logger.debug(
+                "About to execute\n{0}\nagainst:{1}".format(
+                    select_statement,
+                    db_path))
+
+    @staticmethod
+    def _parameters_tostring(parameters):
+        if parameters[0] is datetime:
+            parameters_as_string = ','.join([DateTimeUtils.datetime_to_formatted_string(p) for p in parameters])
+        else:
+            parameters_as_string = ','.join([str(p) for p in parameters])
+        return parameters_as_string
